0f9aac2c9630f46c8f3805dfe7f4b8e7f5918009
SA1FUU <SA1FUU@users.noreply.github.com>
2024-10-10 18:02:12 +0530

    added vercel dot json

--- index.js
+++ index.js
@@ -3,19 +3,20 @@ import express from 'express'
 import dotenv from 'dotenv'
 import cors from 'cors'
 import { urlRoute } from './routes/url.js';
-import { redirectToShortUrl} from './redirect.js';
+import { redirectToShortUrl } from './redirect.js';
 
 dotenv.config()
 const app = express()
 const PORT = process.env.PORT
 
-mongoose.connect(process.env.MONGODB_URI)
+mongoose.connect(process.env.MONGO_LOCAL_SERVER)
     .then(() => console.log('MongoDB Connected'))
     .catch((err) => console.log('MongoDB Connection Error', err));
 
-    
+
 app.use(cors({
     origin: 'https://linktrim-saif.vercel.app',
+    // origin: ' http://localhost:5173',
     credentials: true
 }))
 
